13339e8e52e76bc6230f61d8337dd8a1129bd0b1
Aarni Koskela <akx@iki.fi>
2012-10-07 18:47:56 +0300

    Be more robust (%~dp0 instead of %0 trick)

--- vw.cmd
+++ vw.cmd
@@ -1,5 +1,5 @@
 @echo off
 set VW_ACTSCRIPT_PATH=%TEMP%\vw-%1.cmd
 if EXIST %VW_ACTSCRIPT_PATH% del %VW_ACTSCRIPT_PATH%
-python %0\..\vw-script.py %*
+python %~dp0\vw-script.py %*
 IF EXIST %VW_ACTSCRIPT_PATH% call %VW_ACTSCRIPT_PATH%
